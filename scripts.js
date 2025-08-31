eeff30d285b8f162f4811f04293f3a7c95f70790
swayam03102005 <147474428+swayam03102005@users.noreply.github.com>
2025-08-31 22:44:08 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Activate Vanilla Tilt on all elements with class "tilt"
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+  max: 15,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.2,
+});
